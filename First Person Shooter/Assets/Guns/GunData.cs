18c918e14b030acd5b26aa97cdb668ffcf18ca73
sorrel-kj <93537331+sorrel-kj@users.noreply.github.com>
2024-02-26 16:09:15 -0500

    gun stuff

--- First Person Shooter/Assets/Guns/GunData.cs
+++ First Person Shooter/Assets/Guns/GunData.cs
@@ -8,4 +8,8 @@ public class GunData : ScriptableObject
 {
     public float range = 1000f;
     public int ammo_per_clip = 12;
+    public bool automatic = false;
+    public float primary_fire_delay = 0.5f;
+
+    [Range(0f, 90f)] public float spread = 0.0f;
 }
--- First Person Shooter/Assets/Guns/Pistol.cs
+++ First Person Shooter/Assets/Guns/Pistol.cs
@@ -1,8 +1,10 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class Pistol : MonoBehaviour
 {
@@ -14,6 +16,15 @@ public class Pistol : MonoBehaviour
     //ammo variables
     private int ammo_in_clip;
 
+    //Shooting
+    private bool primary_fire_is_shooting = false;
+    private bool primary_fire_hold = false;
+    private float shoot_delay_timer = 0.0f;
+
+    //Debug
+    public TMP_Text debug_text;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +34,35 @@ public class Pistol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.DrawRay(cam.transform.position, cam.transform.forward * 1000, Color.green);
+        //debug text
+        debug_text.text = "Ammo In Clip" + ammo_in_clip.ToString();
+        PrimaryFire();
+
+        if(shoot_delay_timer > 0) shoot_delay_timer -= Time.deltaTime;
     }
     public void GetPrimaryFireInput(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Started) PrimaryFire();
+        if (context.phase == InputActionPhase.Started)
+        {
+            primary_fire_is_shooting = true;
+        }
+
+        //Check if gun is automatic
+        if (gun_data.automatic)
+        {
+            //Check if hold completed
+            if (context.interaction is HoldInteraction && context.phase == InputActionPhase.Performed)
+            {
+                primary_fire_hold = true;
+            }
+        }
+
+        //Check if button released
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            primary_fire_is_shooting = false;
+            primary_fire_hold = false;
+        }
     }
     public void GetSecondaryFireInput(InputAction.CallbackContext context)
     {
@@ -36,17 +71,30 @@ public class Pistol : MonoBehaviour
 
     private void PrimaryFire()
     {
-        //Raycast
-        ray = new Ray(cam.transform.position, cam.transform.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, gun_data.range))
+        //Check if no delay
+        if (shoot_delay_timer <= 0)
         {
-            Debug.DrawLine(transform.position, hit.point, Color.blue, 0.05f);
+            if (primary_fire_is_shooting || primary_fire_hold)
+            {
+                shoot_delay_timer = gun_data.primary_fire_delay;
+                primary_fire_is_shooting = false;
+
+                Vector3 dir = Quaternion.AngleAxis(Random.Range(-gun_data.spread, gun_data.spread), Vector3.up) * cam.transform.forward;
+                dir = Quaternion.AngleAxis(Random.Range(-gun_data.spread, gun_data.spread), Vector3.right) * dir;
+                //Raycast
+                ray = new Ray(cam.transform.position, dir);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, gun_data.range))
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.blue, 0.05f);
+                }
+                //ammo
+                ammo_in_clip--;
+                if (ammo_in_clip <= 0) ammo_in_clip = gun_data.ammo_per_clip;
+            }
         }
-        //ammo
-        ammo_in_clip--;
-        if (ammo_in_clip <= 0) ammo_in_clip = gun_data.ammo_per_clip;
-        print(ammo_in_clip);
+        
+        
     }
     private void SecondaryFire()
     {
